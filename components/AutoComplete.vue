88fc46a6964f65020744db83ae78882e69e5e02f
realrezasadeghi <96392916+realrezasadeghi@users.noreply.github.com>
2022-01-31 00:23:54 +0330

    Initial commit

---
+++ components/AutoComplete.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="auto-complete">
+    <label for="search">search</label>
+    <input
+      id="search"
+      v-model="search"
+      type="text"
+      class="search-auto-complete"
+      name="search"
+      @input.prevent="searchAutoComplete($event)"
+      @keyup.down="onArrowDown"
+      @keyup.up="onArrowUp"
+      @keypress.enter="onEnter"
+    />
+    <ul v-if="isShowListAutoComplete" class="list-auto-complete">
+      <li
+        v-for="(item, index) in listResult"
+        :key="index"
+        class="item-auto-complete"
+        :class="{ active: index === arrowCounter }"
+        @click="selectAutoComplete(item)"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AutoComplete',
+  props: {
+    listData: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      search: '',
+      listResult: [],
+      isShowListAutoComplete: false,
+      appId: 'c983d82f65c624bedf1c75daf764138c',
+      arrowCounter: -1,
+    }
+  },
+  watch: {
+    search(newVal) {
+      if (newVal === '' || newVal === ' ') {
+        this.isShowListAutoComplete = false
+        this.listResult = []
+      }
+    },
+  },
+  methods: {
+    filterAutoComplete() {
+      this.listResult = this.listData.filter((data) =>
+        data.toLowerCase().includes(this.search.toLowerCase())
+      )
+    },
+    searchAutoComplete(event) {
+      this.isShowListAutoComplete = true
+      this.filterAutoComplete()
+      // const times = []
+      // const time = Date.now()
+    },
+    selectAutoComplete(item) {
+      this.search = item
+      this.isShowListAutoComplete = false
+      setTimeout(this.getInfoCity(), 1000)
+    },
+    onBlur() {
+      this.isShowListAutoComplete = false
+    },
+    onArrowDown() {
+      if (this.arrowCounter < this.listResult.length) {
+        this.arrowCounter += 1
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter -= 1
+      }
+    },
+    onEnter(event) {
+      event.preventDefault()
+      this.search = this.listResult[this.arrowCounter]
+      this.arrowCounter = -1
+      this.isShowListAutoComplete = false
+      setTimeout(this.getInfoCity(), 1000)
+    },
+    async getInfoCity() {
+      const { data } = await this.$axios.get(
+        `/weather?q=${this.search}&appid=${this.appId}`
+      )
+      this.$emit('infoCity', data)
+      this.search = ''
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1 class="title-auto-complete">Enter the name of your city</h1>
+    <auto-complete :list-data="listData" @infoCity="infoCity" />
+    <div v-if="checkData" class="show-info">
+      <h1>Name city : {{ data.name }}</h1>
+      <h2>Country : {{ data.sys.country }}</h2>
+      <h3>cod : {{ data.cod }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import AutoComplete from '~/components/AutoComplete.vue'
+export default {
+  name: 'IndexPage',
+  components: { AutoComplete },
+  data() {
+    return {
+      listData: ['London', 'Tehran', 'Mashhad', 'California'],
+      data: {},
+    }
+  },
+  computed: {
+    checkData() {
+      return Object.keys(this.data).length
+    },
+  },
+  methods: {
+    infoCity(data) {
+      this.data = data
+    },
+  },
+}
+</script>
